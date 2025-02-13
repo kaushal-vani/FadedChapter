763a6845edd280a1f53dcb6031fcb51456b96ecc
kaushal2469 <kaushal2469@gmail.com>
2025-02-13 12:20:39 +0530

    create currency formatter component

---
+++ libs/ui/src/lib/currency-formatter/currency-formatter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-currency-formatter',
+  imports: [CommonModule],
+  templateUrl: './currency-formatter.component.html',
+  styleUrl: './currency-formatter.component.scss',
+})
+export class CurrencyFormatterComponent {}
