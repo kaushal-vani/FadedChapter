e1fc28b91f6d61f341fbea1ed5c8ef4dffdbb1b7
kaushal2469 <kaushal2469@gmail.com>
2025-02-13 11:42:37 +0530

    create feedback lib

---
+++ libs/feedback/src/lib/feedback/feedback.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-feedback',
+  imports: [CommonModule],
+  templateUrl: './feedback.component.html',
+  styleUrl: './feedback.component.scss',
+})
+export class FeedbackComponent {}
