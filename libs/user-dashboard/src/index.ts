5034c64b7bc1d5b4fe2f08c81f2fdaf81d7affae
kaushal2469 <kaushal2469@gmail.com>
2025-02-13 11:43:40 +0530

    create user-dashboard lib

---
+++ libs/user-dashboard/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/user-dashboard/user-dashboard.component';
---
+++ libs/user-dashboard/src/lib/user-dashboard/user-dashboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-user-dashboard',
+  imports: [CommonModule],
+  templateUrl: './user-dashboard.component.html',
+  styleUrl: './user-dashboard.component.scss',
+})
+export class UserDashboardComponent {}
