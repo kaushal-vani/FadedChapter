4811948e0ec122b5cd8ddc9357779b866a0d4052
kaushal2469 <kaushal2469@gmail.com>
2025-02-13 11:45:17 +0530

    export shared components

---
+++ libs/shared/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/components/banner/banner.component';
+export * from './lib/components/footer/footer.component';
+export * from './lib/components/global-nav/global-nav.component';
+export * from './lib/components/slim-nav/slim-nav.component';
