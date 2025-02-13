9f3bc1d88ffbe14fa4f1dbcb624fbd8f480f79e0
kaushal2469 <kaushal2469@gmail.com>
2025-02-13 11:47:09 +0530

    create carousel component

---
+++ libs/ui/src/lib/carousel/carousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-carousel',
+  imports: [CommonModule],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss',
+})
+export class CarouselComponent {}
