bc837b6799f96056d63baaeb1b28e1f2f124e2be
kaushal2469 <kaushal2469@gmail.com>
2025-02-13 11:36:02 +0530

    create global nav component

---
+++ libs/shared/src/lib/components/global-nav/global-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-global-nav',
+  imports: [CommonModule],
+  templateUrl: './global-nav.component.html',
+  styleUrl: './global-nav.component.css',
+})
+export class GlobalNavComponent {}
