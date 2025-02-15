3f5a0fb33c66057e1a36dbccd06bbd0f363e2c42
kaushal2469 <kaushal2469@gmail.com>
2025-02-15 14:29:26 +0530

    create signup component

---
+++ libs/auth/src/lib/components/signup/signup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-signup',
+  imports: [CommonModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss',
+})
+export class SignupComponent {}
