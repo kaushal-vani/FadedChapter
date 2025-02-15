b370168de1a2e8145814576a7c40fe74cef59fa2
kaushal2469 <kaushal2469@gmail.com>
2025-02-15 14:28:11 +0530

    update global nav component

--- libs/shared/src/lib/components/global-nav/global-nav.component.ts
+++ libs/shared/src/lib/components/global-nav/global-nav.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-global-nav',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './global-nav.component.html',
   styleUrl: './global-nav.component.css',
 })
-export class GlobalNavComponent {}
+export class GlobalNavComponent {
+  showSearch = false; // To toggle search box visibility
+  searchQuery = ''; // Bind to the search input field
+
+  toggleSearch(): void {
+    this.showSearch = !this.showSearch; // Toggle visibility of the search box
+  }
+}
