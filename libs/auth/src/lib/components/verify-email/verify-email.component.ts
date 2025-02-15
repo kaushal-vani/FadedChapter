12cb334bb1a43ca3cbad7f6381dbc085d44b8265
kaushal2469 <kaushal2469@gmail.com>
2025-02-15 14:29:45 +0530

    create verify email component

---
+++ libs/auth/src/lib/components/verify-email/verify-email.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-verify-email',
+  imports: [CommonModule],
+  templateUrl: './verify-email.component.html',
+  styleUrl: './verify-email.component.scss',
+})
+export class VerifyEmailComponent {}
