b9f27f2fcf5634a0f178cbe286009860d851cc1e
kaushal2469 <kaushal2469@gmail.com>
2025-02-16 19:25:48 +0530

    create collaborations page component

---
+++ libs/pages/src/lib/collaborations/collaborations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-collaborations',
+  imports: [CommonModule],
+  templateUrl: './collaborations.component.html',
+  styleUrl: './collaborations.component.scss',
+})
+export class CollaborationsComponent {}
