3e9adfb58fa819038e06ff420509137e45c35b9c
kaushal2469 <kaushal2469@gmail.com>
2025-02-16 17:57:40 +0530

    update app and root files

--- apps/store/src/app/app.routes.ts
+++ apps/store/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Route } from '@angular/router';
+import { AboutUsComponent, ContactUsComponent, RefundReturnsComponent, ShippingPaymentsComponent, SizeGuideComponent, TermsPrivacyComponent } from '@faded-chapter/shared';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: 'size-guide', component: SizeGuideComponent },
+  { path: 'shipping-payments', component: ShippingPaymentsComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'contact-us', component: ContactUsComponent },
+  { path: 'refund-returns', component: RefundReturnsComponent },
+  { path: 'privacy-policy', component: TermsPrivacyComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -2,3 +2,11 @@ export * from './lib/components/banner/banner.component';
 export * from './lib/components/footer/footer.component';
 export * from './lib/components/global-nav/global-nav.component';
 export * from './lib/components/slim-nav/slim-nav.component';
+
+
+export * from './lib/static-content/about-us/about-us.component'
+export * from './lib/static-content/contact-us/contact-us.component'
+export * from './lib/static-content/refund-returns/refund-returns.component'
+export * from './lib/static-content/shipping-payments/shipping-payments.component'
+export * from './lib/static-content/size-guide/size-guide.component'
+export * from './lib/static-content/terms-privacy/terms-privacy.component'
