040a75ece2e06b454e067a8f42856d978ad5eeed
kaushal2469 <kaushal2469@gmail.com>
2025-02-16 19:26:04 +0530

    create shop page component

---
+++ libs/pages/src/lib/shop/shop.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-shop',
+  imports: [CommonModule],
+  templateUrl: './shop.component.html',
+  styleUrl: './shop.component.scss',
+})
+export class ShopComponent {}
