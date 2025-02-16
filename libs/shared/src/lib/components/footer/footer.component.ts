1ea81d047e2ff655de8eb9823cd48e915f5d2c47
kaushal2469 <kaushal2469@gmail.com>
2025-02-16 18:18:10 +0530

    fix footer mobile version bug

--- libs/shared/src/lib/components/footer/footer.component.ts
+++ libs/shared/src/lib/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -7,4 +7,17 @@ import { CommonModule } from '@angular/common';
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
-export class FooterComponent {}
+export class FooterComponent implements AfterViewInit {
+
+  ngAfterViewInit() {
+    // Ensure the accordion items are collapsed initially
+    const accordionItems = document.querySelectorAll('.accordion-button');
+
+    accordionItems.forEach(item => {
+      // Check if the item isn't collapsed and force it to be collapsed
+      if (!item.classList.contains('collapsed')) {
+        item.classList.add('collapsed');
+      }
+    });
+  }
+}
