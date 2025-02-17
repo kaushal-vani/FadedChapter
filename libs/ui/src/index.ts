76e81131a6622147e8d9b14db1b3bca83c49bf83
kaushal2469 <kaushal2469@gmail.com>
2025-02-17 22:51:54 +0530

    update index ts

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,3 +1,4 @@
 //Components
 export * from './lib/components/wishlist/wishlist.component'
 export * from './lib/components/cart/cart.component'
+export * from './lib/components/product-card/product-card.component'
--- libs/utils/src/index.ts
+++ libs/utils/src/index.ts
@@ -1,3 +1,18 @@
 //Components
-export * from './lib/components/filter/filter.component'
-export * from './lib/components/sort/sort.component'
+export * from './lib/components/filter/filter.component';
+export * from './lib/components/sort/sort.component';
+
+//Interface
+export * from './lib/models/product/product.interface';
+export * from './lib/models/filter/filter.interface';
+
+//Type
+export * from './lib/types/category/category.type';
+export * from './lib/types/color/color.type';
+export * from './lib/types/fit-type/fit-type.type';
+export * from './lib/types/product-type/product-type.type';
+export * from './lib/types/size/size.type';
+export * from './lib/types/sort/sort.type';
+
+//Mock
+export * from './lib/mocks/product/product.mock';
