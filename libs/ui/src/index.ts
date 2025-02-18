f317310c46286cdb34beaab79ff7d59e898a61b2
kaushal2469 <kaushal2469@gmail.com>
2025-02-18 11:30:45 +0530

    update product component path

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -2,3 +2,8 @@
 export * from './lib/components/wishlist/wishlist.component'
 export * from './lib/components/cart/cart.component'
 export * from './lib/components/product-card/product-card.component'
+
+//Product Components
+export * from './lib/components/product-list/featured-products/featured-products.component'
+export * from './lib/components/product-list/best-selling/best-selling.component'
+export * from './lib/components/product-list/new-arrival/new-arrival.component'
