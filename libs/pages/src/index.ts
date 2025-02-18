c41c2570b701e7d9e731a0483026997a7faab137
kaushal2469 <kaushal2469@gmail.com>
2025-02-18 13:31:46 +0530

    add new arrivals component in index ts

--- libs/pages/src/index.ts
+++ libs/pages/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/home/home.component';
 export * from './lib/collaborations/collaborations.component';
 export * from './lib/shop/shop.component'
+export * from './lib/new-arrivals/new-arrivals.component'
