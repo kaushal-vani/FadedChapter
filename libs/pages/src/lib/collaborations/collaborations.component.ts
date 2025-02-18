3ecc2042fe011f7ac892a48915a7f60eee8f9902
kaushal2469 <kaushal2469@gmail.com>
2025-02-18 12:05:42 +0530

    update collaboration component

--- libs/pages/src/lib/collaborations/collaborations.component.ts
+++ libs/pages/src/lib/collaborations/collaborations.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'lib-collaborations',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './collaborations.component.html',
   styleUrl: './collaborations.component.scss',
 })
-export class CollaborationsComponent {}
+export class CollaborationsComponent {
+  constructor(private router: Router){}
+
+  navigateToShop(){
+    this.router.navigate(['/shop'])
+  }
+}
