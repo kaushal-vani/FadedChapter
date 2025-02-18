d73622fc951fd6f82b460921cd63695d995090a2
kaushal2469 <kaushal2469@gmail.com>
2025-02-18 17:45:56 +0530

    create accessories page component

---
+++ libs/pages/src/lib/accessories/accessories.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-accessories',
+  imports: [CommonModule],
+  templateUrl: './accessories.component.html',
+  styleUrl: './accessories.component.scss',
+})
+export class AccessoriesComponent {}
