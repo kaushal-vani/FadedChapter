8a944e13169cbb5ae99884ad34602f0ce7a87df2
kaushal2469 <kaushal2469@gmail.com>
2025-02-18 13:02:14 +0530

    update home page component

--- libs/pages/src/lib/home/home.component.ts
+++ libs/pages/src/lib/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroBannerComponent } from '@faded-chapter/shared';
+import { BestSellingComponent, FeaturedProductsComponent } from '@faded-chapter/ui';
 
 @Component({
   selector: 'lib-home',
-  imports: [CommonModule,HeroBannerComponent],
+  imports: [CommonModule,HeroBannerComponent, FeaturedProductsComponent,BestSellingComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  activeComponent = 'featured'; // Default view
+
+  showComponent(component: string) {
+    this.activeComponent = component;
+  }
+}
