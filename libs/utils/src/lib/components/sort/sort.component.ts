1a9380d4a8701187fcc492751598fbcb67d5a293
kaushal2469 <kaushal2469@gmail.com>
2025-02-18 17:31:35 +0530

    apply sort functionality

--- libs/utils/src/lib/components/sort/sort.component.ts
+++ libs/utils/src/lib/components/sort/sort.component.ts
@@ -1,10 +1,12 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as bootstrap from 'bootstrap';
+import { SortOption } from '../../types/sort/sort.type';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'lib-sort',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './sort.component.html',
   styleUrl: './sort.component.scss',
 })
@@ -12,6 +14,19 @@ export class SortComponent implements AfterViewInit {
   @ViewChild('offcanvasSort') offcanvasElement!: ElementRef;
   private offcanvasInstance!: bootstrap.Offcanvas;
 
+  @Output() sortChange = new EventEmitter<{ sortOption: SortOption, sortOrder: 'Ascending' | 'Descending' }>();
+
+  selectedSortOption: SortOption = 'Price: Low to High';  // Default sort option
+  selectedSortOrder: 'Ascending' | 'Descending' = 'Ascending'; // Default sort order
+
+  // Emit the selected sort option and order to parent component (Shop)
+  applySort() {
+    this.sortChange.emit({
+      sortOption: this.selectedSortOption,
+      sortOrder: this.selectedSortOrder
+    });
+    this.closeSort()
+  }
   ngAfterViewInit(): void {
     if (this.offcanvasElement) {
       this.offcanvasInstance = new bootstrap.Offcanvas(this.offcanvasElement.nativeElement);
